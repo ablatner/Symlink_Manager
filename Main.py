9f4bfd2ef959d0e12dd468482bb95fd5c64b57ed
ablatner <ablatner@gmail.com>
2013-08-16 23:25:03 -0700

    Moved control creation to own function

--- Main.py
+++ Main.py
@@ -8,35 +8,16 @@ FRAME_POS = (200,200)
 class MainWindow(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, FRAME_TITLE, size = FRAME_SIZE,
-                          style = wx.DEFAULT_FRAME_STYLE)
-        self.Position = FRAME_POS
+                          style = wx.DEFAULT_FRAME_STYLE, pos = FRAME_POS)
+        self.panel = wx.Panel(self) # wx.window that contains ctrls
+        self.panel.BackgroundColour = (250,0,0)
 
-        panel = wx.Panel(self) # wx.window that contains ctrls
-        panel.BackgroundColour = (250,0,0)
-
-        # Heading strings
-        prev_heading = "Previous Links"
-        curr_heading = "Current Links"
-
-        # Create list controls
-        prev_folder_listCtrl = FolderListCtrl(panel)
-        curr_folder_listCtrl = FolderListCtrl(panel)
-
-        # Create buttons
-        match_button = wx.Button(panel, id=-1, label = "Match")
-        new_button = wx.Button(panel, id=-1, label = "New")
-        prev_button = wx.Button(panel, id=-1, label = "Prev")
-        prev_buttons = (match_button, new_button, prev_button)
-
-        unlink_button = wx.Button(panel, id=-1, label = "Unlink")
-        relink_button = wx.Button(panel, id=-1, label = "Relink")
-        curr_buttons = (unlink_button, relink_button)
+        self.create_controls()
 
         # Create column panels
-        prev_panel = ColPanel(panel, prev_heading, prev_folder_listCtrl, prev_buttons)
+        prev_panel = ColPanel(self.panel, self.prev_heading, self.prev_folder_listCtrl, self.prev_buttons)
         prev_panel.BackgroundColour = (0,255,0)
-
-        curr_panel = ColPanel(panel, curr_heading, curr_folder_listCtrl, curr_buttons)
+        curr_panel = ColPanel(self.panel, self.curr_heading, self.curr_folder_listCtrl, self.curr_buttons)
         curr_panel.BackgroundColour = (0,255,255)
 
         col_sizer = wx.BoxSizer(wx.HORIZONTAL) # Makes main column sizer
@@ -46,8 +27,26 @@ class MainWindow(wx.Frame):
         col_sizer.Add(curr_panel, proportion=1, flag=wx.EXPAND) # Adds second list panel
         col_sizer.Add((20,0)) # Adds blank space on right
 
-        panel.SetSizer(col_sizer)
+        self.panel.SetSizer(col_sizer)
         self.Layout()
+
+    def create_controls(self):
+        # Heading strings
+        self.prev_heading = "Previous Links"
+        self.curr_heading = "Current Links"
+        # Folder lists
+        self.prev_folder_listCtrl = FolderListCtrl(self.panel)
+        self.curr_folder_listCtrl = FolderListCtrl(self.panel)
+        # Buttons
+        match_button = wx.Button(self.panel, id=-1, label = "Match")
+        new_button = wx.Button(self.panel, id=-1, label = "New")
+        prev_button = wx.Button(self.panel, id=-1, label = "Prev")
+        self.prev_buttons = (match_button, new_button, prev_button)
+
+        unlink_button = wx.Button(self.panel, id=-1, label = "Unlink")
+        relink_button = wx.Button(self.panel, id=-1, label = "Relink")
+        self.curr_buttons = (unlink_button, relink_button)
+
 
 class ColPanel(wx.Panel):
     def __init__(self, parent, heading_txt, folder_listCtrl, button_tup):
@@ -73,10 +72,9 @@ class ColPanel(wx.Panel):
 class FolderListCtrl(wx.ListCtrl):
     def __init__(self, parent):
         wx.ListCtrl.__init__(self, parent, size=(300,200))
-        self.style = wx.LC_REPORT|wx.BORDER_SUNKEN
+        self.SetSingleStyle(wx.LC_REPORT|wx.BORDER_SUNKEN)
         self.InsertColumn(0, "Folder Name")
         self.InsertColumn(1, "Last Linked Path")
-        self.SetBackgroundColour((255,255,255))
 
 if __name__ == '__main__':
     app = wx.App()
